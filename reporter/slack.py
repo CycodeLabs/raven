1ede267ed928943fc2c6e2eae9667047077b9c0d
elad_pt <124190990+elad-pticha@users.noreply.github.com>
2023-09-24 17:16:26 +0300

    feat: Added simple reporter + slack api to send report (#37)
    * Added simple reporter + slack api to send report.
    
    * Added more detections

---
+++ reporter/slack.py
@@ -0,0 +1,15 @@
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+class Client(object):
+    def __init__(self, token) -> None:
+        self.client = WebClient(token=token)
+
+    def send_message(self, channel_id, message):
+        try:
+            self.client.chat_postMessage(channel=channel_id, text=message)
+            print(f"[x] Report send successfully")
+
+        except SlackApiError as e:
+            print(f"[x] Failed to send report: {e.response['error']}")
