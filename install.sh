e362ada5f671c2ea5adc4c4c666f8b52b659f2d0
Elad Pticha <elad@cycode.com>
2023-10-14 20:15:00 +0300

    Added release workflow.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Global variable to store the tarball URL
 tarball_url=""
+raven_tar="raven.tar.gz"
 
 # Handle errors and exit the script with an error message
 handle_error() {
@@ -13,7 +14,7 @@ handle_error() {
 get_latest_release() {
   local release_url="https://api.github.com/repos/CycodeLabs/raven/releases/latest"
 
-  tarball_url=$(curl -s "$release_url" | jq -r '.tarball_url')
+  tarball_url=$(curl -sSfL "$release_url" | jq -r '.tarball_url')
 
   if [ -z "$tarball_url" ]; then
     handle_error "Failed to extract tarball_url from the latest release"
@@ -22,11 +23,11 @@ get_latest_release() {
 
 # Download the latest release tarball
 download_tarball() {
-  wget -O raven.tar.gz -L "$tarball_url" || handle_error "Failed to download the tarball with wget"
+  wget -O $raven_tar -L "$tarball_url" || handle_error "Failed to download the tarball with wget"
 }
 
 get_latest_release
 download_tarball
 
-tar -xvf raven.tar.gz --one-top-level=raven
-cd raven
+tar -xvf $raven_tar
+cd raven-$(echo $tarball_url | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | tail -1 )
